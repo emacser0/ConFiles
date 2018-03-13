a40ed6568b0b7f0ebfcf95a32e395d2fd4397165
root <root@DESKTOP-I55GPFQ.localdomain>
2018-03-14 02:14:00 +0900

    small changes

--- autoinstaller/install.sh
+++
@@ -1,6 +0,0 @@
-SL=/etc/apt/sources.list
-cp ${SL} ${SL}.org
-sudo sed -e 's/\(us.\)\?archive.ubuntu.com/ftp.daumkakao.com/g' -e 's/security.ubuntu.com/ftp.daumkakao.com/g' < ${SL}.org > ${SL}
-sudo apt-get update
-sudo apt-get install gcc g++ build-essential emacs xfce4 -y
-cp ~/Git/ConFiles/bash/.bash_aliases ~/
