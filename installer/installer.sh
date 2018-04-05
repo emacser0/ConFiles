82466211381101133e15c153b1d4cc54791d633d
ernesti <roxman98@naver.com>
2018-04-05 12:16:25 +0900

    .

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,13 +1,10 @@
-echo "source $HOME/Git/ConFiles/sh/boot.sh" >> $HOME/.bashrc
-echo "source $HOME/Git/ConFiles/sh/boot.sh" >> $HOME/.zshrc
-
-printf "Do you want to use only c/c++?\n"
-read input
 export GITHOME=$HOME/Git
 export CONFHOME=$GITHOME/ConFiles
 export INSTALLERHOME=$CONFHOME/INSTALLER
-export EMACSCONFHOME=$HOME/Git/ConFiles/emacs
-cp $EMACSCONFHOME/.emacs $HOME
+export EMACSCONFHOME=$HOME/Git/ConFiles/emac
+
+printf "Do you want to use only c/c++?\n"
+read input
 
 if [ $input = "y" ]; then
     cd $EMACSCONFHOME
@@ -17,17 +14,31 @@ if [ $input = "y" ]; then
     mv backup elisp
 fi
 
-echo "export DISPLAY=:0.0" >> $HOME/.bashrc
-echo "export DISPLAY=:0.0" >> $HOME/.zshrc
-
 sudo sed -i 's/archive.ubuntu.com/ftp.neowiz.com/g' /etc/apt/sources.list
 sudo apt-get update
 
 sudo apt-get install -y gcc
 sudo apt-get install -y g++
 sudo apt-get install -y clang
-sudo apt-get install -y build-essential
 sudo apt-get install -y emacs
 sudo apt-get install -y guake
 sudo apt-get install -y xfce4
+sudo apt-get install -y zsh
+sudo apt-get install -y fonts-powerline
+cp $EMACSCONFHOME/.emacs $HOME
+curl -L https://git.io/epre | sh
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+printf "Are you using Windows Subsystem for Linux?\n"
+read input
+
+if [ $input = "y" ]; then
+    echo "source $HOME/Git/ConFiles/sh/boot.sh" >> $HOME/.bashrc
+    echo "source $HOME/Git/ConFiles/sh/boot.sh" >> $HOME/.zshrc
+fi
+
+echo "export DISPLAY=:0.0" >> $HOME/.bashrc
+echo "export DISPLAY=:0.0" >> $HOME/.zshrc
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' $HOME/.zshrc
