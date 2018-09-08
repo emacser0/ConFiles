9844be356aa88e7aff5606b217f3add56d760a72
emacser <roxman98@naver.com>
2018-09-09 00:59:26 +0900

    .

--- emacs/elisp/cpp.el
+++ emacs/elisp/cpp.el
@@ -1,9 +1,5 @@
 (require 'company-c-headers)
-(add-hook 'c++-mode-hook
-          (lambda () (setq flycheck-clang-language-standard "c++11")))
-(add-to-list 'company-c-headers-path-system "/usr/include/c++/5.4.0/")
-(add-to-list 'company-c-headers-path-system "/usr/include/x86_64-linux-gnu/c++/5.4.0/")
-
-
-
-
+(add-to-list 'company-c-headers-path-system "/usr/include/c++/7.3.0/")
+(add-to-list 'company-c-headers-path-system "/usr/include/x86_64-linux-gnu/")
+(add-to-list 'company-c-headers-path-system "/usr/include/x86_64-linux-gnu/c++/7.3.0/")
+(setq c-basic-offset 2)
